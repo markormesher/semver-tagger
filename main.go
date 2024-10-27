87f52eae60a730862671af71ea5d9d09cd6d484a
Mark Ormesher <me@markormesher.co.uk>
2024-10-27 11:02:05 +0000

    fix long/short flag declaration

--- main.go
+++ main.go
@@ -32,39 +32,41 @@ var defaultBranches = []string{"main", "master", "develop"}
 
 func main() {
 	// config
-	autoFlag := flag.Bool("a", false, "")
-	majorFlag := flag.Bool("M", false, "")
-	minorFlag := flag.Bool("m", false, "")
-	patchFlag := flag.Bool("p", false, "")
-	rcFlag := flag.Bool("rc", false, "")
-	initFlag := flag.Bool("init", false, "")
-	verboseFlag := flag.Bool("v", false, "")
-	forceFlag := flag.Bool("f", false, "")
-	forceFlag = flag.Bool("force", false, "")
-	noConfirmFlag := flag.Bool("y", false, "")
-	noConfirmFlag = flag.Bool("no-confirm", false, "")
-	pushFlag := flag.Bool("P", false, "")
-	pushFlag = flag.Bool("push", false, "")
+
+	var autoFlag, majorFlag, minorFlag, patchFlag, rcFlag, initFlag, verboseFlag, forceFlag, noConfirmFlag, pushFlag bool
+	flag.BoolVar(&autoFlag, "a", false, "")
+	flag.BoolVar(&majorFlag, "M", false, "")
+	flag.BoolVar(&minorFlag, "m", false, "")
+	flag.BoolVar(&patchFlag, "p", false, "")
+	flag.BoolVar(&rcFlag, "rc", false, "")
+	flag.BoolVar(&initFlag, "init", false, "")
+	flag.BoolVar(&verboseFlag, "v", false, "")
+	flag.BoolVar(&forceFlag, "f", false, "")
+	flag.BoolVar(&forceFlag, "force", false, "")
+	flag.BoolVar(&noConfirmFlag, "y", false, "")
+	flag.BoolVar(&noConfirmFlag, "no-confirm", false, "")
+	flag.BoolVar(&pushFlag, "P", false, "")
+	flag.BoolVar(&pushFlag, "push", false, "")
 
 	flag.Usage = func() { fmt.Println(usage) }
 	flag.Parse()
 
-	if *verboseFlag {
+	if verboseFlag {
 		log.Verbose = true
 	}
 
 	// ensure only one version type flag was passed
 	qtyTagTypeFlags := 0
-	if *autoFlag {
+	if autoFlag {
 		qtyTagTypeFlags++
 	}
-	if *majorFlag {
+	if majorFlag {
 		qtyTagTypeFlags++
 	}
-	if *minorFlag {
+	if minorFlag {
 		qtyTagTypeFlags++
 	}
-	if *patchFlag {
+	if patchFlag {
 		qtyTagTypeFlags++
 	}
 	if qtyTagTypeFlags > 1 {
@@ -72,7 +74,7 @@ func main() {
 		os.Exit(1)
 	} else if qtyTagTypeFlags == 0 {
 		log.Debug("No tag set; assuming -a")
-		*autoFlag = true
+		autoFlag = true
 	}
 
 	// validate the repo state
@@ -83,7 +85,7 @@ func main() {
 		os.Exit(1)
 	}
 	if !repoClean {
-		if *forceFlag {
+		if forceFlag {
 			log.Warn("Repo is not clean, but continuing because force flag is specified")
 		} else {
 			log.Error("Repo is not clean")
@@ -97,7 +99,7 @@ func main() {
 		os.Exit(1)
 	}
 	if !slices.Contains(defaultBranches, currentBranch) {
-		if *forceFlag {
+		if forceFlag {
 			log.Warn("Current branch (%s) doesn't look like a default branch, but continuing because force flag is specified", currentBranch)
 		} else {
 			log.Error("Current branch (%s) doesn't look like a default branch", currentBranch)
@@ -109,9 +111,9 @@ func main() {
 
 	description, err := git.Describe()
 	if err != nil {
-		if *initFlag {
+		if initFlag {
 			log.Info("Creating the initial tag")
-			err = git.CreateTag(&semver.SemVer{Prefix: "v", Major: 0, Minor: 1, Patch: 0}, *noConfirmFlag)
+			err = git.CreateTag(&semver.SemVer{Prefix: "v", Major: 0, Minor: 1, Patch: 0}, noConfirmFlag)
 			if err != nil {
 				log.Error("%v", err)
 				os.Exit(1)
@@ -129,7 +131,7 @@ func main() {
 	}
 
 	if currentVer.CommitDistance == 0 {
-		if *forceFlag {
+		if forceFlag {
 			log.Warn("There have been no commits since the last tag, but continuing because force flag is specified")
 		} else {
 			log.Info("There have been no commits since the last tag")
@@ -141,41 +143,41 @@ func main() {
 
 	newVer := currentVer
 
-	if *autoFlag {
+	if autoFlag {
 		// TODO: build this
 		log.Error("Auto-tag isn't built yet. Sorry")
 		os.Exit(1)
 	}
 
 	switch {
-	case *majorFlag:
+	case majorFlag:
 		newVer.Major++
 		newVer.Minor = 0
 		newVer.Patch = 0
 
-	case *minorFlag:
+	case minorFlag:
 		newVer.Minor++
 		newVer.Patch = 0
 
-	case *patchFlag:
+	case patchFlag:
 		newVer.Patch++
 	}
 
-	if *rcFlag {
+	if rcFlag {
 		newVer.Rc++
 	}
 
 	log.Info("Prev:  %s", currentVer.String())
 	log.Info("New:   %s", newVer.String())
 
-	err = git.CreateTag(&newVer, *noConfirmFlag)
+	err = git.CreateTag(&newVer, noConfirmFlag)
 	if err != nil {
 		log.Error("%v", err)
 		os.Exit(1)
 	}
 	log.Info("Created tag")
 
-	if *pushFlag {
+	if pushFlag {
 		err = git.PushTags()
 		if err != nil {
 			log.Error("%v", err)

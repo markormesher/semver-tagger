b2b2710f94f04a943c9cee07ee64c339979cde13
Mark Ormesher <me@markormesher.co.uk>
2024-10-27 11:34:28 +0000

    add test for String()

--- internal/semver/main_test.go
+++ internal/semver/main_test.go
@@ -4,28 +4,28 @@ import "testing"
 
 func TestFromString(t *testing.T) {
 	tests := map[string]struct {
-		input  string
-		expect SemVer
+		input    string
+		expected SemVer
 	}{
 		"simple, with prefix": {
-			input:  "v1.2.3",
-			expect: SemVer{Prefix: "v", Major: 1, Minor: 2, Patch: 3, Rc: 0, CommitDistance: 0},
+			input:    "v1.2.3",
+			expected: SemVer{Prefix: "v", Major: 1, Minor: 2, Patch: 3, Rc: 0, CommitDistance: 0},
 		},
 		"simple, no prefix": {
-			input:  "1.2.3",
-			expect: SemVer{Prefix: "", Major: 1, Minor: 2, Patch: 3, Rc: 0, CommitDistance: 0},
+			input:    "1.2.3",
+			expected: SemVer{Prefix: "", Major: 1, Minor: 2, Patch: 3, Rc: 0, CommitDistance: 0},
 		},
 		"release candidate": {
-			input:  "1.2.3-rc4",
-			expect: SemVer{Prefix: "", Major: 1, Minor: 2, Patch: 3, Rc: 4, CommitDistance: 0},
+			input:    "1.2.3-rc4",
+			expected: SemVer{Prefix: "", Major: 1, Minor: 2, Patch: 3, Rc: 4, CommitDistance: 0},
 		},
-		"commit description": {
-			input:  "1.2.3-5-gabcdef",
-			expect: SemVer{Prefix: "", Major: 1, Minor: 2, Patch: 3, Rc: 0, CommitDistance: 5},
+		"commit distance": {
+			input:    "1.2.3-5-gabcdef",
+			expected: SemVer{Prefix: "", Major: 1, Minor: 2, Patch: 3, Rc: 0, CommitDistance: 5},
 		},
-		"release candidate and commit description": {
-			input:  "1.2.3-rc4-5-gabcdef",
-			expect: SemVer{Prefix: "", Major: 1, Minor: 2, Patch: 3, Rc: 4, CommitDistance: 5},
+		"release candidate and commit distance": {
+			input:    "1.2.3-rc4-5-gabcdef",
+			expected: SemVer{Prefix: "", Major: 1, Minor: 2, Patch: 3, Rc: 4, CommitDistance: 5},
 		},
 	}
 
@@ -36,8 +36,46 @@ func TestFromString(t *testing.T) {
 			if err != nil {
 				t.Fatalf("FromString(%q) failed: %q", test.input, err)
 			}
-			if actual != test.expect {
-				t.Fatalf("FromString(%q) returned %q; expected %q", test.input, &actual, &test.expect)
+			if actual != test.expected {
+				t.Fatalf("FromString(%q) returned %q; expected %q", test.input, &actual, &test.expected)
+			}
+		})
+	}
+}
+
+func TestString(t *testing.T) {
+	tests := map[string]struct {
+		input    SemVer
+		expected string
+	}{
+		"simple, with prefix": {
+			input:    SemVer{Prefix: "v", Major: 1, Minor: 2, Patch: 3, Rc: 0, CommitDistance: 0},
+			expected: "v1.2.3",
+		},
+		"simple, no prefix": {
+			input:    SemVer{Prefix: "", Major: 1, Minor: 2, Patch: 3, Rc: 0, CommitDistance: 0},
+			expected: "1.2.3",
+		},
+		"release candidate": {
+			input:    SemVer{Prefix: "", Major: 1, Minor: 2, Patch: 3, Rc: 4, CommitDistance: 0},
+			expected: "1.2.3-rc4",
+		},
+		"commit distance": {
+			input:    SemVer{Prefix: "", Major: 1, Minor: 2, Patch: 3, Rc: 0, CommitDistance: 5},
+			expected: "1.2.3",
+		},
+		"release candidate and commit distance": {
+			input:    SemVer{Prefix: "", Major: 1, Minor: 2, Patch: 3, Rc: 4, CommitDistance: 5},
+			expected: "1.2.3-rc4",
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			actual := test.input.String()
+			if actual != test.expected {
+				t.Fatalf("String(%+v) returned %q; expected %q", test.input, actual, test.expected)
 			}
 		})
 	}
